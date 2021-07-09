a1ea95bd61de751c6180c380d688bb92425b4b56
HT <hoangthanh@gxd.vn>
2021-07-09 00:42:00 -0700

    123

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display:["group-hover"],
+      margin:["group-hover"],
+      visibility:["group-hover"],
+
+    },
+  },
+  plugins: [],
+}
